ab287fe6015afb696b0f07157ed3ce8a415dfd5a
kensuuu <kensuke.11288@gmail.com>
2023-05-11 00:57:31 +0900

    format

--- medium/GroupAnagrams/Solution.java
+++ medium/GroupAnagrams/Solution.java
@@ -27,7 +27,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(solution.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
         System.out.println(solution.groupAnagrams(new String[]{""}));
         System.out.println(solution.groupAnagrams(new String[]{"a"}));
     }
